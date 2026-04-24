a079d6f7ad3ed1a586a8ccf9b79ed440d37f1e1e
Ryan Jung <rjung@thunderbird.net>
2026-04-24 12:17:21 -0600

    Remove all code related to fluent-bit

--- fluent-bit/filesystem/fluent-bit/buffers/README.md
+++
@@ -1,4 +0,0 @@
-# Fluent-Bit Buffers
-
-This directory stores events which are too large to store in the configured memory buffers, or which
-are in queue to be shipped to an output.
--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -9,8 +9,6 @@ this is *not* a `tb_pulumi <https://github.com/thunderbird/pulumi>`_ project as 
 of any of those larger infrastructure patterns.
 """
 
-import pulumi
-import pulumi_cloudflare as cloudflare
 import tb_pulumi
 import tb_pulumi.cloudwatch
 import tb_pulumi.iam
@@ -23,61 +21,9 @@ from site24x7 import main as site24x7
 
 project = tb_pulumi.ThunderbirdPulumiProject()
 global_config = project.config.get('config', {})
-resources = project.config.get('resources', {})
 
 # Some feature flags
 build_site24x7 = global_config.get('build_site24x7', False)
-build_tbpulumi = global_config.get('build_tbpulumi', False)
 
 if build_site24x7:
     site24x7()
-
-if build_tbpulumi:
-    psm_opts = resources.get('tb:secrets:PulumiSecretsManager', {}).get('secrets')
-    psm = tb_pulumi.secrets.PulumiSecretsManager(
-        name=f'{project.name_prefix}-secrets',
-        project=project,
-        **psm_opts,
-    )
-
-    logdest_opts = resources.get('tb:cloudwatch:LogDestination', {})
-    logdests = {
-        logdest_name: tb_pulumi.cloudwatch.LogDestination(
-            f'{project.name_prefix}-logdest-{logdest_name}',
-            project=project,
-            app_name=logdest_name,
-            **logdest_config,
-        )
-        for logdest_name, logdest_config in logdest_opts.items()
-    }
-
-    vpc_config = resources.get('tb:network:MultiCidrVpc', {}).get('fluentbit', {})
-    vpc_fluentbit = tb_pulumi.network.MultiCidrVpc(
-        f'{project.name_prefix}-vpc-fluentbit',
-        project=project,
-        **vpc_config,
-    )
-
-    ecs_clusters = {
-        cluster_name: tb_pulumi.fargate.AutoscalingFargateCluster(
-            f'{project.name_prefix}-fargate-{cluster_name}',
-            project=project,
-            subnets=vpc_fluentbit.resources.get('subnets', []),
-            **cluster_config,
-            opts=pulumi.ResourceOptions(depends_on=[vpc_fluentbit]),
-        )
-        for cluster_name, cluster_config in resources.get(
-            'tb:fargate:AutoscalingFargateCluster'
-        ).items()
-    }
-
-    cloudflare_zone_id = project.pulumi_config.require_secret('cloudflare_zone_id')
-    fluent_bit_dns = cloudflare.DnsRecord(
-        f'{project.name_prefix}-dns-fluentbit',
-        name='fluentbit' if project.stack == 'prod' else f'fluentbit-{project.stack}',
-        content=ecs_clusters['fluentbit'].resources['load_balancers']['fluentbit-http'].dns_name,
-        ttl=60,
-        type='CNAME',
-        zone_id=cloudflare_zone_id,
-        opts=pulumi.ResourceOptions(depends_on=[*ecs_clusters.values()]),
-    )
